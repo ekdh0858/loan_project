6a4db66e7f1bd3dd2ebeaeee782338202461a20f
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2024-09-24 21:58:18 +0900

    feat: add terms of service registration
    이용 약관 등록 기능 구현

--- src/main/java/com/fastcampus/loan/repository/TermsRepository.java
+++ src/main/java/com/fastcampus/loan/repository/TermsRepository.java
@@ -1,9 +1,10 @@
 package com.fastcampus.loan.repository;
 
-import com.fastcampus.loan.domain.Counsel;
+import com.fastcampus.loan.domain.Terms;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CounselRepository extends JpaRepository<Counsel, Long> {
+public interface TermsRepository extends JpaRepository<Terms, Long> {
+
 }
